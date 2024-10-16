f9e1f6ddc6da9ddb2580d27552c021ff24a6c201
Takiiiiiiii <43725718+Takiiiiiiii@users.noreply.github.com>
2024-10-16 23:02:10 +0800

    Remove Linux

--- linux/_samba.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SAMBA_USER=root
-SAMBA_PASS=1
-
-#==================================================================
-(echo $SAMBA_PASS; echo $SAMBA_PASS;) | smbpasswd -a -s $SAMBA_USER
